1e642cdfdd4b6d1068ee0c38e3397ac726e0cbe4
Freibuis <freibuis@gmail.com>
2014-07-22 21:24:35 +1000

    Fixed DND & tidy up formating

--- lib/avaya/user_info.rb
+++ lib/avaya/user_info.rb
@@ -1,38 +1,41 @@
 module Avaya
   class UserInfo
-    attr_reader :user, :login_name, :full_name, :ext,
-                :forward_unconditional, :forward_unconditional_number, :forward_unconditional_all_calls,
-                :forward_me_to, :forward_busy, :dnd, :voicemail
+    attr_reader :raw,
+                :user,
+                :login_name,
+                :full_name,
+                :ext,
+                :forward_unconditional,
+                :forward_unconditional_number,
+                :forward_unconditional_all_calls,
+                :forward_busy,
+                :dnd
 
-    def initialize(user)
-      @search = user
-      get
-
+    def initialize(ext)
+      @ext = ext
     end
 
-    def self.get (user)
-      user_info = self.new(user)
+    def self.get (ext)
+      user_info = self.new(ext)
+      user_info.get
       user_info
     end
 
     def get
-      @user = Avaya::TFTP.read(:"user_info#{3}", @search)
-      @user = @user[0].split(',')
+      @raw       = Avaya::TFTP.read(:"user_info#{3}", @ext)
+      @user_info = @raw[0].split(',')
 
-      @login_name = @user[0]
-      @full_name = @user[1]
-      @ext = @user[2]
-      @forward_busy = @user[3] == "1" ? true : false
-      @forward_no_answer = @user[4] == "1" ? true : false
-      @forward_unconditional = @user[5] == "1" ? true : false
-      @forward_unconditional_number = @user[6]
-      @dnd = @user[7] == "1" ? true : false
-
-      @forward_unconditional_all_calls = @user[25] == "1" ? true : false
-      @forward_me_to = @user[8] == "1" ? true : false
-      @voicemail_to_email = @user[11] == "1" ? true : false
-      @voicemail_count = @user[11]
-      "updated"
+      @login_name                      = @user_info[0]
+      @full_name                       = @user_info[1]
+      @ext                             = @user_info[2]
+      @forward_busy                    = @user_info[3] == "1" ? true : false
+      @forward_no_answer               = @user_info[4] == "1" ? true : false
+      @forward_unconditional           = @user_info[5] == "1" ? true : false
+      @forward_unconditional_number    = @user_info[6]
+      @dnd                             = @user_info[8] == "1" ? true : false
+      @forward_unconditional_all_calls = @user_info[25] == "1" ? true : false
+      @voicemail_count                 = @user_info[11]
+      self
     end
 
 
