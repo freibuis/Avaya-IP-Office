db23468fb3992613d2f0d711ec66649ceaacf6d3
Freibuis <freibuis@gmail.com>
2014-07-25 23:45:28 +1000

    refactor + remove crap

--- lib/avaya/user_info.rb
+++ lib/avaya/user_info.rb
@@ -12,7 +12,13 @@ module Avaya
                 :dnd,
                 :logged_in,
                 :connected_party,
-                :phone_state
+                :phone_state,
+                :absent_option,
+                :absent_extra_info,
+                :absent_enabled,
+                :ext_template,
+                :twinning_number,
+                :twinning_enabled
 
     def initialize(ext)
       @ext = ext
@@ -30,23 +36,23 @@ module Avaya
 
       @login_name                      = @user_info[0]
       @full_name                       = @user_info[1]
-      @ext                             = @user_info[2]
+      @ext                             = Integer(@user_info[2])
       @forward_busy                    = @user_info[3] == "1"
-      @forward_no_answer               = @user_info[4] == "1" ? true : false
-      @forward_unconditional           = @user_info[5] == "1" ? true : false
+      @forward_no_answer               = @user_info[4] == "1"
+      @forward_unconditional           = @user_info[5] == "1"
       @forward_unconditional_number    = @user_info[6]
-      @dnd                             = @user_info[8] == "1" ? true : false
-      #Phone State "no" on hook ,"og" off hook
+      @dnd                             = @user_info[8] == "1"
+      #Phone State "no" onhook ,"og" Out Going,"in" Incoming
       @phone_state                     = @user_info[19]
       @connected_party                 = @user_info[20]
-      @forward_unconditional_all_calls = @user_info[25] == "1" ? true : false
-      @logged_in                       = @user_info[44] == "1" ? true : false
+      @forward_unconditional_all_calls = @user_info[25] == "1"
+      @logged_in                       = @user_info[44] == "1"
       @absent_option                   = Integer(@user_info[37])
       @absent_extra_info               = @user_info[38]
-      @absent_enabled                  = @user_info[39] == "1" ? true : false
+      @absent_enabled                  = @user_info[39] == "1"
       @ext_template                    = @user_info[53]
       @twinning_number                 = @user_info[67]
-      @twinning_enabled                = @user_info[68]== "1" #? true : false
+      @twinning_enabled                = @user_info[68]== "1"
       self
     end
 
